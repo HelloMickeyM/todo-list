5fb19d73391e59847881ae291f98cabb15eb83ed
Rex <2584805132@qq.com>
2022-07-05 12:05:48 +0800

    修改test1.vue新增data函数

--- src/components/test1.vue
+++ src/components/test1.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <h1>测试模板1234</h1>
-    <h1>master-test1</h1>
+    <h2>master-test1</h2>
+    <h3>{{测试}}</h3>
   </div>
-  
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      name:'测试'
+    }
+  },
 }
 </script>
 
